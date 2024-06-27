27bfdd59326bf57ea60e43a64dc775fe71313a86
therishabhshrivastava <you@example.com>
2024-06-27 20:02:14 +0530

    Implemented a responsive navigation bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,71 @@
-
 import React, { useState } from "react";
+import React, { useState } from "react";
+import Logo from "../Assets/Logo.png";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import { Box, Drawer, List, Divider, ListItem, ListItemButton, ListItemIcon, ListItemText }  from "@mui/material";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon />,
+      link: '#home',
+    },
+    {
+      text: "About",
+      icon: <InfoIcon />,
+      link: '#about',
+    },
+    {
+      text: "Testimonials",
+      icon: <CommentRoundedIcon />,
+      link: '#testimonial',
+    },
+    {
+      text: "Contact",
+      icon: <PhoneRoundedIcon />,
+      link: '#contact',
+    },
+  ];
   return (
     <nav>
-     
+      <div className="nav-logo-container">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="navbar-links-container">
+         {menuOptions.map((item, index) => (
+          <a key={index} href={item.link}>{item.text}</a>
+        ))}
+        <button className="primary-button">Bookings Now</button>
+      </div>
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((item) => (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </Box>
+      </Drawer>
     </nav>
   );
 };
