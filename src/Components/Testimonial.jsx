a1d25444cefb03293d79bdcd1c84d9aee6fb43e4
therishabhshrivastava <you@example.com>
2024-06-27 20:19:18 +0530

    added cutomers testimonials in testimonial section

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import ProfilePic from "../Assets/profile-image.png";
+import { AiFillStar } from "react-icons/ai";
 
 const Testimonial = () => {
   return (
     <div className="work-section-wrapper" id="testimonial">
-      
+      <div className="work-section-top">
+        <p className="primary-subheading">Testimonial</p>
+        <h1 className="primary-heading">What They Are Saying</h1>
+      </div>
+      <div className="testimonial-section-bottom">
+        <img src={ProfilePic} alt="" />
+        <p>
+            Thank you for dinner last night. It was amazing!! I have to say it’s the best meal I have had in quite some time. You will definitely be seeing more of me eating at your establishment. My Wife was very impressed and we can’t wait for our parents to come visit so that we can share our new favorite place with them.
+        </p>
+        <div className="testimonials-stars-container">
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+        </div>
+        <h2>Rohit Mehra</h2>
+      </div>
     </div>
   );
 };
